8fcd0d7704bdd11f93daa0ab11a654d70a46c99f
Jangey <jangey.lu@gmail.com>
2019-06-18 22:40:56 -0700

    Zoom to San Francisco by default

--- Ridecell-Demo/Ridecell-Demo/ViewController.swift
+++ Ridecell-Demo/Ridecell-Demo/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // show user current location
+        self.mapView.showsUserLocation = true
+        getSF_Location()
+    }
+    
+    func getSF_Location()  {
+        // Zoom to San Francisco by default
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let newLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(37.7749, -122.4194)
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: newLocation, span: span)
+        mapView.setRegion(region, animated: true)
     }
 
 
